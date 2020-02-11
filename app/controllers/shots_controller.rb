2b9c196ec99b629e79cdcc13f9b89879e9763e11
codershona <falgunimst95@gmail.com>
2020-02-11 20:26:01 +0100

    javascript is not worrking , that why trying to fix my drag and drop

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,10 +1,12 @@
 class ShotsController < ApplicationController
   before_action :set_shot, only: [:show, :edit, :update, :destroy]
 
+before_action :authenticate_user!, only: [:edit, :update, :destroy]
+
   # GET /shots
   # GET /shots.json
   def index
-    @shots = Shot.all
+    @shots = Shot.all.order('created_at DESC')
   end
 
   # GET /shots/1
@@ -14,7 +16,7 @@ class ShotsController < ApplicationController
 
   # GET /shots/new
   def new
-    @shot = Shot.new
+    @shot = current_user.shots.build
   end
 
   # GET /shots/1/edit
@@ -24,7 +26,7 @@ class ShotsController < ApplicationController
   # POST /shots
   # POST /shots.json
   def create
-    @shot = Shot.new(shot_params)
+    @shot = current_user.shots.build(shot_params)
 
     respond_to do |format|
       if @shot.save
